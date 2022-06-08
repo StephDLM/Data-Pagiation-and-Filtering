1388161c7004e6392f4f20e55dabde2ed3b93c6b
StephDLM <106278655+StephDLM@users.noreply.github.com>
2022-06-08 14:57:30 -0700

    updated Where's the Action warm-up, has some bugs still

--- data-pagination-and-filtering-v1/js/script.js
+++ data-pagination-and-filtering-v1/js/script.js
@@ -18,6 +18,10 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
+function showPage (){
+
+
+};
 
 
 /*
@@ -25,6 +29,11 @@ Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
+function addPagination (){
+
+};
 
 
 // Call functions
+showPage();
+addPagination();
