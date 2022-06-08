d563b0e19b224c5c1ef6477de3a5f30421172f5d
StephDLM <106278655+StephDLM@users.noreply.github.com>
2022-06-08 10:10:47 -0700

    unit 2 warm up 2, stuck on functions

--- Warm-up/list-pagination-whats-the-deal-with-functions/js/script.js
+++ Warm-up/list-pagination-whats-the-deal-with-functions/js/script.js
@@ -43,18 +43,30 @@ const value4 = [value1, value2, value3]
   // Using the variables created for you above:
   // 1a. Set the `style.color` property of `element1` to green, or your favorite color
   // 1b. Set the `innerHTML` property of `element1` equal to the `number` parameter
+  function func1 (number){
+    element1.style.color = purple
+    element1.innerHTML = number
+  };
 
 
 // 2. Create a function named `func2` - it should accept a single parameter called `string`
   // Using the variables created for you above:
   // 2a. Set the `style.color` property of `element2` to green, or your favorite color
   // 2b. Set the `innerHTML` property of `element2` equal to the `string` parameter
+  function func2 (string){
+    element2.style.color = green
+    element2.innerHTML = string
+  };
 
 
 // 3. Create a function named `func3` - it should accept a single parameter called `element`
   // Using the variables created for you above:
   // 3a. Set the `style.color` property of `element3` to green, or your favorite color
   // 3b. Set the `innerHTML` property of `element3` equal to the `element` parameter
+  function func3 (element){
+    element3.style.color = skyblue
+    element3.innerHTML = element
+  };
 
 
 // 4. Create a function named `func4` - it should accept a single parameter called `array`
@@ -64,6 +76,14 @@ const value4 = [value1, value2, value3]
   // 4c. Loop over the `array` parameter
     // 4cc. For each loop iteration, concatenate `array[i]` to the `myString` variable you created above
   // 4d. Set the `innerHTML` of `element4` equal to the `myString`
+  function func4 (array){
+    element4.style.color = salmon
+    const myString = '';
+    for (let i=0; i<array.length; i++) {
+      //array[i].myString
+    }
+    element4.innerHTML = myString
+  };
 
 
 // 5. Call each function you've created in the associated event listeners below,
@@ -83,7 +103,7 @@ const value4 = [value1, value2, value3]
 
 /* btn1 listener */
 btn1.addEventListener('click', () => {
-
+  func1(value1);
   // Invoke `func1` here and pass in the `value1` argument
 
   // Helpful log statement to test function
@@ -95,6 +115,7 @@ btn1.addEventListener('click', () => {
 btn2.addEventListener('click', () => {
 
   // Invoke `func2` here and pass in the `value2` argument
+  func2(value2);
 
   // Helpful log statement to test function
   console.log('Second button is functional!');
@@ -105,6 +126,7 @@ btn2.addEventListener('click', () => {
 btn3.addEventListener('click', () => {
 
   // Invoke `func3` here and pass in the `value3` argument
+  func3(value3);
 
   // Helpful log statement to test function
   console.log('Third button is functional!');
@@ -115,6 +137,7 @@ btn3.addEventListener('click', () => {
 btn4.addEventListener('click', () => {
 
   // Invoke `func4` here and pass in the `value4` argument 
+  func4(value4);
 
   // Helpful log statement to test function
   console.log('Fourth button is functional!');
